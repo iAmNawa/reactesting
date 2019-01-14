83045342aeef7ad4503bce5bee3f2ea10e73f16e
Paul Borawski <paulborawski14@gmail.com>
2019-01-14 11:51:39 -0800

    added renderHeader() method

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 import CommentBox from './CommentBox';
 import CommentList from './CommentList';
 
-export default () => {
-  return (
-    <div>
-      <Route path="/post" component={CommentBox} />
-      <Route path="/" exact component={CommentList} />    
-    </div>
-  );
+export default class App extends Component {
+  renderButton() {}
+
+  renderHeader() {
+    return (
+      <ul>
+        <l1>
+          <Link to="/">Home</Link>
+        </l1>
+        <l1>
+          <Link to="/post">Post A Comment</Link>
+        </l1>
+        <l1>{this.renderButton()}</l1>
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <Route path="/post" component={CommentBox} />
+        <Route path="/" exact component={CommentList} />
+      </div>
+    );
+  }
 };
