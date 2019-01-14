212aeca651b592c90c5296e9b258b5dd9c6be949
Paul Borawski <paulborawski14@gmail.com>
2019-01-14 11:56:52 -0800

    renderHeader is being rendered with proper Sign In or Sign Out

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import CommentBox from './CommentBox';
 import CommentList from './CommentList';
 
-export default class App extends Component {
-  renderButton() {}
+class App extends Component {
+  renderButton() {
+    if (this.props.auth) {
+      return (
+        <button>
+          Sign out
+        </button>
+      );
+    } else {
+      return (
+        <button>
+          Sign in
+        </button>
+      )
+    }
+  }
 
   renderHeader() {
     return (
@@ -23,9 +38,16 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        {this.renderHeader()}
         <Route path="/post" component={CommentBox} />
         <Route path="/" exact component={CommentList} />
       </div>
     );
   }
 };
+
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
+export default connect(mapStateToProps)(App);
