3ed318c4f9fa559c4109d5f6366c820fda3c6a47
Paul Borawski <paulborawski14@gmail.com>
2019-01-17 12:56:59 -0800

    using debugger

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -5,8 +5,9 @@ export default function(state = [], action) {
     case SAVE_COMMENT:
       return [...state, action.payload];
     case FETCH_COMMENTS:
+      debugger;
       const comments = action.payload.data.map(comment => comment.name);
-      return [...state, ...comments] 
+      return [...state, ...comments]
     default:
       return state;
   }
