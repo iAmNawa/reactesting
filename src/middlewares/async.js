169ac26832434a797af314bbbdeb4b7611c18b5d
Paul Borawski <paulborawski14@gmail.com>
2019-01-18 16:11:57 -0800

    writing own middleware

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -2,4 +2,9 @@ export default ({ dispatch }) => next => action  => {
   if (!action.payload || !action.payload.then) {
     return next(action);
   }
+  // There is a promise on our payload property
+  action.payload.then(function(response) {
+    const newAction = { ...action, payload: response };
+    dispatch(newAction);
+  });
 };
