a195aa92bfe9e8444fd81dfdb3e79cff2f67c93d
Paul Borawski <paulborawski14@gmail.com>
2019-01-19 08:21:41 -0800

    stateValidator middleware testing valid state

--- src/middlewares/stateValidator.js
+++ src/middlewares/stateValidator.js
@@ -1,3 +1,8 @@
-export default ({ dispatch }) => (next) => (action) => {
+import tv4 from 'tv4';
+import stateSchema from './stateSchema';
+
+export default ({ dispatch, getState }) => (next) => (action) => {
   next(action);
+
+  console.log(tv4.validate(getState(), stateSchema));
 };
